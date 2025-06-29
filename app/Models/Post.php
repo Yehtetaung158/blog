0b05cd5adb340f9d4db6b217ed31be4d22828abc
Yehtetaung158 <137622241+Yehtetaung158@users.noreply.github.com>
2025-06-30 00:00:12 +0630

    first

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'user_id',
+        'published_at',
+        'updated_at',
+    ];
+    protected $dates = [
+        'published_at',
+        'updated_at',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
